099703148567c80e83b504bb3fc15fbeb0e57e15
grubk <grubert.kai@gmail.com>
2025-07-12 20:15:18 -0700

    removed debugging comment

--- src/utils/translator.ts
+++ src/utils/translator.ts
@@ -5,7 +5,6 @@ export async function translateText(
 ): Promise<string | null> {
   const encodedText = encodeURIComponent(text);
   const url = `https://grubk-lingvatranslate.vercel.app/api/v1/${sourceLang}/${targetLang}/${encodedText}`;
-  //const url = `https://grubk-lingvatranslate.vercel.app/api/v1/en/fr/greetings`;
 
   console.log("Translation URL:", url); // Debug log to verify URL format
 
